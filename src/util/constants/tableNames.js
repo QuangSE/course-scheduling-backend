f9176554ab824c0f40f91cc164f5112c5068c7d4
Quang <do_quang-tung@web.de>
2022-05-28 17:09:02 +0200

    feat: add table names as constants

---
+++ src/util/constants/tableNames.js
@@ -0,0 +1,14 @@
+module.exports = {
+    comment: "comment",
+    course: "course",
+    docent: "docent",
+    docent_course: "docent_course",
+    exam_regulations: "exam_regulations",
+    major: "major",
+    major_group: "major_group",
+    module: "module",
+    module_major_group: "module_major_group",
+    notice: "notice",
+    permission: "permission",
+    user: "user",
+}
