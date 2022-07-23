8e7173cfffa2a1528f007deedcffdb1c54c5cea1
Quang <do_quang-tung@web.de>
2022-07-23 03:12:09 +0200

    doc: add todo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv").config();
-const http = require("http");
+const http = require("http"); //TODO: setup https server
 
 const app = require("./app");
 const logger = require("./util/logger");
